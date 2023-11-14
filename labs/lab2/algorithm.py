49c6172772f582aa06fd4b554d1c1a569c08d87b
Rostislav Nikolaev <rostislaev@yandex.ru>
2023-11-14 15:34:15 +0700

    Fix algorithm + add greedy approximate algorithm for comparison

--- labs/lab2/algorithm.py
+++ labs/lab2/algorithm.py
@@ -1,38 +1,55 @@
+import numpy as np
+
+
 def add_item_to_configuration(prev_config, new_item_index):
     next_config = prev_config.copy()
     next_config[new_item_index] += 1
     return next_config
 
 
-def solve(max_weight, total_items, weights, costs):
-    dynamic_table = [max_weight + 1]
-    configurations = [max_weight + 1]
+def solveDynamicaly(max_weight, total_items, weights, costs):
+    dynamic_table = [[0 for _ in range(max_weight + 1)] for _ in range(total_items)]
 
-    dynamic_table[0] = 0 if weights[0] > 1 else costs[0]
-    configurations[0] = [0] * total_items
+    for w in range(max_weight):
+        dynamic_table[0][w] = costs[0] if weights[0] <= w else 0
 
-    for w in range(1, max_weight + 1):
-        max_cost = 0
-        added_item = None
-        prev_configuration = None
-        for i in range(total_items):
-            space_left = w - weights[i]
-            cur_cost = dynamic_table[space_left] + costs[i] if space_left > -1 else 0
-            if cur_cost > max_cost:
-                max_cost = cur_cost
-                added_item = i
-                prev_configuration = configurations[space_left]
-        if added_item is None or prev_configuration is None:
-            dynamic_table.append(0)
-            configurations.append([0] * total_items)
-            continue
-        dynamic_table.append(max_cost)
-        new_config = add_item_to_configuration(prev_configuration, added_item)
-        configurations.append(new_config)
+    for item in range(1, total_items):
+        item_weight = weights[item]
+        item_cost = costs[item]
+        for w in range(max_weight + 1):
+            pick_cost = dynamic_table[item - 1][w - item_weight] + item_cost if item_weight <= w else 0
+            skip_cost = dynamic_table[item - 1][w]
+            dynamic_table[item][w] = max(pick_cost, skip_cost)
 
     # print(dynamic_table)
     # print(configurations)
     #
-    # print("Best cost : " + str(dynamic_table[-1]))
-    # print("Best configuration : " + str(configurations[-1]))
-    return dynamic_table[-1], configurations[-1]
+    configuration = [0] * total_items
+    capacity = max_weight
+    item_i = total_items - 1
+    while capacity > 0 and item_i > 0:
+        if dynamic_table[item_i][capacity] != dynamic_table[item_i - 1][capacity]:
+            configuration[item_i] = 1
+            capacity -= weights[item_i]
+        item_i -= 1
+    if capacity > weights[item_i]:
+        configuration[0] = 1
+    return dynamic_table[-1][-1], configuration
+
+
+def solveGreedy(max_weight, total_items, weights):
+    weights = np.array(weights)
+    indexes = np.array([i for i in range(total_items)])
+    sorted_weight_indexes = weights.argsort()
+    indexes_sorted = indexes[sorted_weight_indexes[::-1]]
+    weights_sorted = weights[sorted_weight_indexes[::-1]]
+
+    current_weight = 0
+    optimal_configuration = np.zeros(total_items)
+    for i in range(len(weights_sorted)):
+        w = weights_sorted[i]
+        new_weight = current_weight + w
+        if new_weight <= max_weight:
+            current_weight += w
+            optimal_configuration[indexes_sorted[i]] = 1
+    return current_weight, optimal_configuration
--- labs/lab2/test.py
+++ labs/lab2/test.py
@@ -1,6 +1,12 @@
-from algorithm import solve
+from algorithm import solveDynamicaly, solveGreedy
 
-test_files = ['data/a_example.in', 'data/b_small.in', 'data/c_medium.in', 'data/d_quite_big.in', 'data/e_also_big.in']
+test_files = [
+    'data/a_example.in',
+    'data/b_small.in',
+    'data/c_medium.in',
+    # 'data/d_quite_big.in',
+    # 'data/e_also_big.in'
+]
 
 for input_file_path in test_files:
     with open(input_file_path, 'r') as file:
@@ -9,16 +15,20 @@ for input_file_path in test_files:
     input = input.strip()
     input = input.split(" ")
 
+    print('TEST ' + input_file_path)
+
     W = int(input[0])
     total_items = int(input[1])
     weights = [int(x) for x in input[2:]]
     costs = [int(x) for x in input[2:]]  # weights are also costs here
 
-    # print(str(W))
+    print('Max weight is : ' + str(W))
     # print(str(total_items))
-    # print(str(weights))
+    print(str(weights))
 
-    result_cost, result_config = solve(W, total_items, weights, costs)
-    print(str(result_cost))
-    print(str(result_config))
-    break
+    result_cost, result_config = solveDynamicaly(W, total_items, weights, costs)
+    # result_cost, result_config = solveGreedy(W, total_items, weights)
+    print('Best weight is : ' + str(result_cost))
+    print('Best config is : ' + str(result_config))
+    print("\n\n\n")
+    # break
