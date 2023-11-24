c362f6afa917a9e64f099bce431643943af40a1c
Rostislav Nikolaev <rostislaev@yandex.ru>
2023-11-24 20:12:40 +0700

    Extract class for annealing simulation

--- labs/lab10/annealing.py
+++ labs/lab10/annealing.py
@@ -4,8 +4,13 @@ import random
 random.seed(42)
 
 
-def anneal(cities: [City], temperature : int):
-    # random.shuffle(cities)
-    starting_route = TSPRoute(cities)
-    print('Starting route is : ' + str(starting_route))
-    return starting_route
+class AnnealingSimulator:
+    def __init__(self, cities: [City], starting_temperature: float = 100, cool_down_coef: float = 0.95):
+        self.temperature = starting_temperature
+        self.cool_down_coef = cool_down_coef
+        random.shuffle(cities)
+        self.route = TSPRoute(cities)
+        print('Starting route is : ' + str(self.route))
+
+    def anneal(self):
+        pass
