b5868337751e5386b12e6be9e5fc5e68bd069b73
Rostislav Nikolaev <rostislaev@yandex.ru>
2023-11-24 12:29:33 +0700

    Implement interfaces for City and TSPRoute + getting 2opt-neighbour

---
+++ labs/lab10/tsp.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+class City:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distance(self, other):
+        return np.linalg.norm((self.x - other.x, self.y - other.y))
+
+
+class TSPRoute:
+    def __init__(self, route: [City]):
+        self.route = route
+
+    def get_2opt_neighbour(self, first_index, second_index):
+        new_route = []
+        for c in self.route[:first_index]:
+            new_route.append(c)
+        for i in range(second_index - 1, first_index, -1):
+            new_route.append(self.route[i])
+        for c in self.route[second_index:]:
+            new_route.append(c)
+
+        return TSPRoute(new_route)
