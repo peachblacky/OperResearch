b3645c939d7e86dc305b7eddc8074e86efcb4a7e
Rostislav Nikolaev <rostislaev@yandex.ru>
2023-11-27 13:52:25 +0700

    First working verion

--- labs/lab10/annealing.py
+++ labs/lab10/annealing.py
@@ -1,3 +1,5 @@
+import math
+
 from tsp import TSPRoute, City
 import random
 import numpy as np
@@ -15,7 +17,9 @@ class AnnealingSimulator:
 
     def anneal(self):
         active = True
-        while active:
+        best_route = self.route
+        # while active:
+        for epoch in range(1000):
             for _ in range(self.route.cities_number):
                 neighbour = self.select_random_neighbour()
                 cur_route_length = self.route.route_length()
@@ -24,11 +28,22 @@ class AnnealingSimulator:
                 if delta >= 0:
                     self.route = neighbour
                 else:
-                    #TODO
-                    pass
+                    exponential_power = delta / self.temperature
+                    if random.random() < math.exp(exponential_power):
+                        self.route = neighbour
+                if self.route.route_length() < best_route.route_length():
+                    best_route = self.route
+            self.temperature *= self.cool_down_coef
+            if epoch % 10 == 0:
+                self.print_progress(epoch, best_route)
+        return best_route
+
+    def print_progress(self, epoch, cur_best_route: TSPRoute):
+        print('Epoch ' + str(epoch) + ' Temp: ' + str(self.temperature) + '. Cur. best: ' + str(cur_best_route.route_length()))
 
     def select_random_neighbour(self):
         swapping_indexes = np.random.choice(self.route.cities_number, 2)
+        swapping_indexes.sort()
         return self.route.get_2opt_neighbour(swapping_indexes[0], swapping_indexes[1])
 
     def __calculate_starting_temperature(self):
--- labs/lab10/test.py
+++ labs/lab10/test.py
@@ -5,6 +5,10 @@ from annealing import AnnealingSimulator
 if __name__ == '__main__':
     cities = data.read_data()
     # print([str(c) for c in cities])
-    algo = AnnealingSimulator(cities)
+    algo = AnnealingSimulator(cities, starting_temperature=200000, cool_down_coef=0.99)
     print(algo.route.route_length())
     # algo.route.draw_route()
+    solution = algo.anneal()
+    print(str(solution))
+    print(solution.route_length())
+    solution.draw_route()
