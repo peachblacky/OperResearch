06ce2db9b5bf8c5779b10f309053c43807f1f886
Rostislav Nikolaev <rostislaev@yandex.ru>
2023-11-27 14:04:11 +0700

    Fix random + add proper calculation of starting temperatures

--- labs/lab10/annealing.py
+++ labs/lab10/annealing.py
@@ -5,15 +5,17 @@ import random
 import numpy as np
 
 random.seed(42)
+np.random.seed(42)
 
 
 class AnnealingSimulator:
-    def __init__(self, cities: [City], starting_temperature: float = 100, cool_down_coef: float = 0.95):
-        self.temperature = starting_temperature
+    def __init__(self, cities: [City], starting_temperature: float = -1, cool_down_coef: float = 0.95):
         self.cool_down_coef = cool_down_coef
         random.shuffle(cities)
         self.route = TSPRoute(cities)
+        self.temperature = self.__calculate_starting_temperature() if starting_temperature < 0 else starting_temperature
         print('Starting route is : ' + str(self.route))
+        print('Starting temperature is : ' + str(self.temperature))
 
     def anneal(self):
         active = True
@@ -47,5 +49,15 @@ class AnnealingSimulator:
         return self.route.get_2opt_neighbour(swapping_indexes[0], swapping_indexes[1])
 
     def __calculate_starting_temperature(self):
-        # TODO max distance to the neighbour of the starting_route
-        pass
+        max_delta = 0
+        for i in range(self.route.cities_number - 1):
+            for j in range(i + 1, self.route.cities_number):
+                neighbour = self.route.get_2opt_neighbour(i, j)
+                cur_route_length = self.route.route_length()
+                neighbour_route_length = neighbour.route_length()
+                delta = cur_route_length - neighbour_route_length
+                if delta > max_delta:
+                    max_delta = delta
+        return max_delta
+
+
--- labs/lab10/test.py
+++ labs/lab10/test.py
@@ -5,7 +5,7 @@ from annealing import AnnealingSimulator
 if __name__ == '__main__':
     cities = data.read_data()
     # print([str(c) for c in cities])
-    algo = AnnealingSimulator(cities, starting_temperature=200000, cool_down_coef=0.99)
+    algo = AnnealingSimulator(cities, cool_down_coef=0.99)
     print(algo.route.route_length())
     # algo.route.draw_route()
     solution = algo.anneal()
