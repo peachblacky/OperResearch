ae93f285ac05117adfc8eafdcacca811c3258e0c
Rostislav Nikolaev <rostislaev@yandex.ru>
2023-11-28 12:59:04 +0700

    Basic functionality works fine

--- labs/lab18/tabu_search.py
+++ labs/lab18/tabu_search.py
@@ -1,21 +1,22 @@
 import random
-
 import numpy as np
+from placement import City
 
 np.random.seed(42)
 
 
 class TabuSearcher:
-    def __init__(self, cities, cost_per_shop = 3000, neighbourhood_probability=0.25):
+    def __init__(self, cities: [City], cost_per_shop=3000, neighbourhood_probability=0.25):
         self.cities = cities
+        self.cities_number = len(cities)
         self.neighbourhood_probability = neighbourhood_probability
 
-        self.cur_combination = np.zeros(100)
+        self.cur_combination = np.zeros(self.cities_number)
         self.generate_starting_combination()
 
         self.cost_per_shop = cost_per_shop
 
     def generate_starting_combination(self):
-        number_of_shops = np.random.randint(0, 101)
-        shop_indexes = set(np.random.randint(0, 100, number_of_shops))
-        self.cur_combination[shop_indexes] = 1
+        number_of_shops = np.random.randint(0, self.cities_number // 10)
+        shop_indexes = set(np.random.randint(0, self.cities_number, number_of_shops))
+        self.cur_combination[list(shop_indexes)] = 1
--- labs/lab18/test.py
+++ labs/lab18/test.py
@@ -1,6 +1,9 @@
 import data
-import tabu_search
+from tabu_search import TabuSearcher
 
 if __name__ == '__main__':
     cities, cost_per_shop = data.read_data('data/euq/111.txt')
     print(cities[0].client_distances)
+    searcher = TabuSearcher(cities)
+    print(searcher.cur_combination)
+
