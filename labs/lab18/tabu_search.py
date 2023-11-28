48c9c29a1a38131ffa0fa7c7dfc970f9acc95666
Rostislav Nikolaev <rostislaev@yandex.ru>
2023-11-28 18:52:23 +0700

    Add tabu update + start implementing solve method

--- labs/lab18/tabu_search.py
+++ labs/lab18/tabu_search.py
@@ -9,12 +9,14 @@ np.random.seed(42)
 class TabuSearcher:
     cur_combination: TabuSearchCombination
 
-    def __init__(self, cities: [City], cost_per_shop=3000, neighbourhood_probability=0.25, neighbourhood_radius=2, history_length=5):
+    def __init__(self, cities: [City], cost_per_shop=3000, neighbourhood_probability=0.25, neighbourhood_radius=2,
+                 history_length=5):
         self.cities = cities
         self.cities_number = len(cities)
         self.cost_per_shop = cost_per_shop
         self.neighbourhood_probability = neighbourhood_probability
         self.neighbourhood_radius = neighbourhood_radius
+        self.history_length = history_length
 
         self.generate_starting_combination()
 
@@ -22,14 +24,21 @@ class TabuSearcher:
         self.tabu_list = deque()
 
     def solve(self):
-        pass
+        active = True
+        best_route = self.cur_combination
+        epoch = 0
+        unchanged_iterations = 0
+        while active:
+            # TODO
+            pass
 
     def is_tabu(self, indexes: [int]):
         return indexes in self.tabu_list
 
-    def update_tabu(self, indexes: [int]):
+    def update_tabu_v1(self, indexes: [int]):
         self.tabu_list.append(indexes)
-        # if len(self.tabu_list) >
+        if len(self.tabu_list) > self.history_length:
+            self.tabu_list.popleft()
 
     def generate_starting_combination(self):
         combination = np.zeros(self.cities_number, dtype='int')
@@ -38,11 +47,11 @@ class TabuSearcher:
         combination[list(shop_indexes)] = 1
         self.cur_combination = TabuSearchCombination(combination)
 
-    def calculate_cost(self):
+    def calculate_cost(self, combination : TabuSearchCombination):
         cost = 0
-        cur_combination_bool = self.cur_combination.combination != 0
+        cur_combination_bool = combination.combination != 0
         for city_index in range(self.cities_number):
-            if self.cur_combination.combination[city_index] == 1:
+            if combination.combination[city_index] == 1:
                 cost += self.cost_per_shop
             else:
                 cost += self.cities[city_index].find_nearest_shop_distance(cur_combination_bool)
@@ -52,6 +61,6 @@ class TabuSearcher:
         neighbourhood = []
         for i in range(self.cities_number - 1):
             for j in range(i + 1, self.cities_number):
-                if np.random.random() < self.neighbourhood_probability:
+                if np.random.random() < self.neighbourhood_probability and not self.is_tabu([i, j]):
                     neighbourhood.append(self.cur_combination.get_hamming_neighbour([i, j]))
         return neighbourhood
